a2d25bf01daec3c2695a59b799fa9fc5e07ad7fd
cowlgns1213 <38933008+cowlgns1213@users.noreply.github.com>
2023-03-27 23:12:18 +0900

    SignUp route create

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,85 @@
+import logo from "../logo.svg";
+import paper from "../assets/paper.gif";
+import loading from "../assets/loading.png";
+import "../App.css";
+import { useState } from "react";
+import { Link as OriginalLink } from "react-router-dom";
+
+export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+
+  const [emailValid, setEmailValid] = useState(false);
+  const [pwValid, setPwValid] = useState(false);
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    const regex = /^[0-9]+$/;
+    if (regex.test(email)) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+    }
+  };
+
+  const handlePassWord = (e) => {
+    setPw(e.target.value);
+    const regex = /^[0-9]+$/;
+    if (regex.test(pw)) {
+      setPwValid(true);
+    } else {
+      setPwValid(false);
+    }
+  };
+  return (
+    <div className="host">
+      <div className="titleImage">
+        <img src={paper} />
+      </div>
+      <div className="titleImage2">
+        <img src={loading} />
+      </div>
+
+      <div className="contentWrap">
+        <div className="inputWrap">
+          <div className="idinput">ID</div>
+          <input
+            className="input"
+            type="text"
+            value={email}
+            onChange={handleEmail}
+          />
+        </div>
+
+        <div className="errorMessageWrap">
+          {!emailValid && email.length > 0 && (
+            <div>올바른 이메일을 입력해주세요.</div>
+          )}
+        </div>
+
+        <div className="inputWrap">
+          <div className="PWinput">PW</div>
+          <input
+            className="input"
+            type="password"
+            value={pw}
+            onChange={handlePassWord}
+          />
+        </div>
+
+        <div className="errorMessageWrap">
+          {!pwValid && pw.length > 0 && (
+            <div> 영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
+          )}
+        </div>
+
+        <div>
+          <button className="bottomButton"> SIGN IN </button>
+          <OriginalLink to="/signup">
+            <button className="bottomButton2"> SING UP </button>
+          </OriginalLink>
+        </div>
+      </div>
+    </div>
+  );
+}
